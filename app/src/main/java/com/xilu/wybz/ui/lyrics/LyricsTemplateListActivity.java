036e147781ef177f9ce84de06a9dc45e7c461dbd
mogumodel <mogumodel@126.com>
2016-08-04 11:53:32 +0800

    configs

---
+++ app/src/main/java/com/xilu/wybz/ui/lyrics/LyricsTemplateListActivity.java
@@ -0,0 +1,23 @@
+package com.xilu.wybz.ui.lyrics;
+
+import android.view.ViewGroup;
+
+import com.xilu.wybz.ui.base.BaseListActivity;
+import com.xilu.wybz.view.pull.BaseViewHolder;
+
+/**
+ * Created by Administrator on 2016/8/3.
+ */
+public class LyricsTemplateListActivity extends BaseListActivity{
+
+
+    @Override
+    protected void initPresenter() {
+
+    }
+
+    @Override
+    protected BaseViewHolder getViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/xilu/wybz/ui/lyrics/MakeWordByTempleateActivity.java
@@ -0,0 +1,25 @@
+package com.xilu.wybz.ui.lyrics;
+
+import android.os.Bundle;
+
+import com.xilu.wybz.ui.base.ToolbarActivity;
+
+/**
+ * Created by Administrator on 2016/8/3.
+ */
+public class MakeWordByTempleateActivity extends ToolbarActivity {
+
+
+    @Override
+    protected int getLayoutRes() {
+        return 0;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+
+}
