b0dd7ee5c99558c137cacd0c876960665bcd375f
mogumodel <mogumodel@126.com>
2016-08-28 09:24:09 +0800

    布局文件修改

--- app/src/main/java/com/xilu/wybz/ui/find/FindActivity.java
+++ app/src/main/java/com/xilu/wybz/ui/find/FindActivity.java
@@ -13,7 +13,6 @@ import com.xilu.wybz.ui.base.BasePlayMenuActivity;
 import com.xilu.wybz.ui.fragment.LyricsFragment;
 import com.xilu.wybz.ui.fragment.RankingFragment;
 import com.xilu.wybz.ui.fragment.SongFragment;
-import com.xilu.wybz.view.SystemBarHelper;
 
 import butterknife.Bind;
 import butterknife.OnClick;
