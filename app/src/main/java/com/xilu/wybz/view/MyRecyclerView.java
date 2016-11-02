ae5c59da0be61b627d0951e20eabddc3a1756ccd
yangguosong <15603484446@163.com>
2016-11-02 14:45:44 +0800

    myrecycler

---
+++ app/src/main/java/com/xilu/wybz/view/MyRecyclerView.java
@@ -0,0 +1,42 @@
+package com.xilu.wybz.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by Administrator on 2016/10/27.
+ */
+
+public class MyRecyclerView extends RecyclerView {
+
+
+
+    public MyRecyclerView(Context context) {
+        super(context);
+    }
+
+    public MyRecyclerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthSpec, int heightSpec) {
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
+                MeasureSpec.AT_MOST);
+        super.onMeasure(widthSpec, expandSpec);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+
+
+        return false;
+    }
+}
